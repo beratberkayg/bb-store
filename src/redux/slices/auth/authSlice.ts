b8758fba5eaad57bc5cb74f67cf4a17549ce3b67
beratberkayg <berat.1675@gmail.com>
2023-12-13 14:53:19 +0300

    create authSlice

---
+++ src/redux/slices/auth/authSlice.ts
@@ -0,0 +1,65 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { auth, db } from "@/services/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+
+interface type {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const initialState: type = {
+  name: "",
+  email: "",
+  password: "",
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ name, email, password }: type) => {
+    const user = await createUserWithEmailAndPassword(auth, email, password);
+    await updateProfile(user.user, { displayName: name });
+    setDoc(doc(db, "users", email), {
+      name,
+      email,
+      password,
+    });
+  }
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ email, password }: type) => {
+    await signInWithEmailAndPassword(auth, email, password);
+  }
+);
+
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await signOut(auth);
+});
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    changeName: (state, action) => {
+      state.name = action.payload;
+    },
+    changeEmail: (state, action) => {
+      state.email = action.payload;
+    },
+    changePassword: (state, action) => {
+      state.password = action.payload;
+    },
+  },
+});
+
+export const { changeName, changeEmail, changePassword } = authSlice.actions;
+
+export default authSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import dataSlice from "./slices/dataSlice";
 import counterSlice from "./slices/counter/counterSlice";
 import cartSlice from "./slices/cart/cartSlice";
+import authSlice from "./slices/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     data: dataSlice,
     counter: counterSlice,
     carts: cartSlice,
+    auth: authSlice,
   },
 });
 
